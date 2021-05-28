ae6650ed4582001ed6a04215e92f15e77fba669a
Buzhak <Byzhak@ya.ru>
2021-05-29 02:52:29 +0300

    first_part_of_task

--- process_data.py
+++ process_data.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from datetime import datetime
+import csv
 """
 Дополнительное задание.
 
@@ -22,12 +25,62 @@ from pathlib import Path
 reports = Path('./data').glob('*.csv')
 for file in reports:
     print(report.name)
-
 """
+def file_data(file_name):
+    
+    file_date = datetime.strptime(file_name, '%d%m%Y')
+    return file_date.strftime('%d %m %Y')
+
+def weight_to_float(item):
+    item = item['Weight']
+    item = item.replace('%', '')
+    item = float(item.replace(',', '.'))
+    return item
+    
 
 
 def process_data() -> None:
-    pass
+    reports = Path('./data').glob('*.csv')
+    for file in reports:
+        file_name = file.name
+        file_name = file_name[:-4]
+        file_name = file_name.split('_')
+        print(f'Название фонда: "{file_name[0]} {file_name[1]}" Дата: {file_data(file_name[-1])}')
+
+        
+        with open ('./data/'+file.name, 'r', encoding='utf-8') as file:
+
+            titles = file.readline() 
+            titles = titles.replace('\n', '')
+            titles = titles.split(',')
+            
+            
+            if len(titles) == 5:
+
+                fields = [titles[0],titles[1],titles[2],titles[3],titles[4]]
+            else:
+                fields = [titles[0],titles[1],titles[2],titles[3]]
+            
+
+            reader = csv.DictReader(file, fields, delimiter = ',')
+            reader = sorted(reader, key = weight_to_float, reverse = True)
+            print('Топ 10 самых крупных компонентов фонда:')
+            for row in range(1, 11):
+                try:
+                    print(reader[row]['Weight'])
+                except(IndexError):
+                    break
+
+            sum_weight_str = list(i for i in reader)
+            sum_weight = sum(map(weight_to_float, sum_weight_str))
+            print(f"Фаил заполнен на {round(sum_weight, 2)}%")
+            print()
+        
+
+        
+        
+    
+
 
 
 if __name__ == '__main__':
